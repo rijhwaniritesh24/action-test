3f84ee89aab1f991eba8b69daa8e3eb707f24c6b
Ritesh Rijhwani <rijhwaniritesh24@gmail.com>
2025-02-15 19:51:44 +0530

    Workflow changes

---
+++ .github/script/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Example to check if two strings are equal or not
+# first string
+a="Hello"
+b="Hello"
+if test "$a" = "$b"
+then
+   echo "a is equal to b"
+else
+   echo "a is not equal to b"
+fi
